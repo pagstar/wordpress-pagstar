2a5875389625947891c8d4969e3c882a54be5305
WillBorgesDev <73146988+WillBorgesDev@users.noreply.github.com>
2025-05-02 02:55:17 -0300

    fix: enabled pagstar

--- class-pagstar-gateway.php
+++ class-pagstar-gateway.php
@@ -62,7 +62,7 @@ class WC_Pagstar_Gateway extends WC_Payment_Gateway
     $this->title = $this->get_option('title');
     $this->description = $this->get_option('description');
     $this->instructions = $this->get_option('instructions');
-    $this->enabled = $this->get_option('enabled');
+    $this->enabled = 'yes';
 
     // Ações
     add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
