faef877a677a998bce168007adb1831b70ed6ebb
WillBorgesDev <73146988+WillBorgesDev@users.noreply.github.com>
2025-05-16 00:55:40 -0300

    feat: changes in new params

--- pagstar-woocommerce-plugin.php
+++ pagstar-woocommerce-plugin.php
@@ -49,11 +49,8 @@ add_action('woocommerce_admin_order_data_after_order_details', function ($order)
     $txid = $order->get_meta('_pagstar_txid');
     
     if ($end2end) {
-        echo '<div class="order_data_column">
-                <h3>' . __('End2End ID', 'pagstar') . '</h3>
-                <p>
-                    <textarea readonly style="width:100%;resize:none;background-color:#f3f3f3;border-radius:3px;padding:8px;border:1px solid #ddd;">' . esc_html($end2end) . '</textarea>
-                </p>
+        echo '<div class="address">
+                <p><strong>' . __('End2End ID', 'pagstar') . ':</strong><br>' . esc_html($end2end) . '</p>
               </div>';
     }
 
